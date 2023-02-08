4ee3327ca668b22309ce94fddd88e3e602cf22f2
jucian0 <juciano@outlook.com.br>
2023-02-08 08:31:15 -0300

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-acme`
-  extends: ["acme"],
+  // This tells ESLint to load the config from the package `eslint-config-jucian0`
+  extends: ["jucian0"],
   settings: {
     next: {
       rootDir: ["apps/*/"],
